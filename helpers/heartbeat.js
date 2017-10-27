354f8e6e9efe69c211dcdca431fa9a253eb85c2b
Bryan Tong <contact@nullivex.com>
2017-10-27 15:30:05 -0600

    try to update using cas

--- helpers/heartbeat.js
+++ helpers/heartbeat.js
@@ -343,10 +343,23 @@ var markMeUp = function(systemKey,systemPrism,systemType,done){
   })
   var downKey = couch.schema.downVote(systemKey)
   debug('Getting peer information',key)
-  return couch.peer.upsertAsync(key,{active: true, available: true})
-    .catch(function(err){
-      debug('failed to mark peer up',err)
-    })
+  return couch.peer.getAsync(key)
+    .then(
+      function(result){
+        var peer = result.value
+        debug('Got peer information back',peer)
+        peer.available = true
+        peer.active = true
+        return couch.peer.upsertAsync(key,peer,{cas: result.cas})
+          .catch(function(err){
+            debug('failed to mark peer up',err)
+          })
+      },
+      function(err){
+        debug('Got an error getting peer information',err)
+        throw new Error('Could not get peer information, cannot mark myself up')
+      }
+    )
     .then(function(){
       //Time to delete the downvote log
       var qstring = 'DELETE FROM ' +
