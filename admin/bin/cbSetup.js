08050437fadd6e671aa2cdadb3cf6392d0b633b0
Bryan Tong <contact@nullivex.com>
2017-11-07 16:57:11 -0700

    try a different setup order for better timing

--- admin/bin/cbSetup.js
+++ admin/bin/cbSetup.js
@@ -11,19 +11,19 @@ var roles = []
 couch.createBuckets()
   .then(function(result){
     roles = result
+    console.log('Bucket creation complete')
+    console.log('Creating user')
+    return couch.createUser(config.couch.username,config.couch.password,roles)
+  })
+  .then(function(){
     var waitSeconds = 5
-    if(process.env.TRAVIS) waitSeconds = 30
-    console.log('Bucket creation complete, waiting ' + waitSeconds + ' seconds')
+    if(process.env.TRAVIS) waitSeconds = 60
+    console.log('User creation complete, waiting ' + waitSeconds + ' seconds')
     return new P(function(resolve){
       setTimeout(resolve,waitSeconds * 1000)
     })
   })
   .then(function(){
-    console.log('Creating user')
-    return couch.createUser(config.couch.username,config.couch.password,roles)
-  })
-  .then(function(){
-    console.log('User creation complete')
     console.log('Creating indexes ')
     return couch.createIndexes()
   })
